04d827217c8ce42068c75e5f597b400d034c63ef
Bennet-Ukoh <bennetukoh@gmail.com>
2022-09-21 21:51:47 -0700

    lists.h added

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -8,7 +8,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- *
+ * 
  */
 
 typedef struct listint_s
@@ -18,8 +18,11 @@ typedef struct listint_s
 } listint_t;
 
 size_t print_listint(const listint_t *h);
+
 listint_t *add_nodeint(listint_t **head, const int n);
+
 void free_listint(listint_t *head);
+
 int check_cycle(listint_t *list);
 
 #endif /* LISTS_H */
