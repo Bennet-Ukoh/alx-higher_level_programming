7470f73f552aad25d9f447509846b73d82376c16
Bennet-Ukoh <bennetukoh@gmail.com>
2022-10-14 08:46:15 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# 1-square.py by Bennet Ukoh
+"""Defines a square"""
+
+
+class Square:
+    """
+    The class defines a square.
+
+    
+    Attribute:
+        size (int) : The size of the square.
+
+    """
+
+
+    def __init__(self, size)
+    """Initialization of the size of the square as a private attribute."""
+    self.__size = size
