68c3c77964e4ccd4601c9aabd04938b10437be0c
Bennet-Ukoh <bennetukoh@gmail.com>
2022-10-14 09:13:41 -0700

    indentation errors fixed

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -7,15 +7,13 @@ class Square:
     """
     The class defines a square.
 
-    
+
     Attribute:
         size (int) : The size of the square.
 
     Args:
         size (int): Takes in the given size of the square.
-
     """
 
-
-    def __init__(self, size)
-    self.__size = size
+    def __init__(self, size):
+        self.__size = size
