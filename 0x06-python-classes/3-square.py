2fd75124c46773dbd8e6a5452881d8c4cabc00df
Bennet-Ukoh <bennetukoh@gmail.com>
2022-10-14 22:28:39 -0700

    blank spaces formatted

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -12,7 +12,8 @@ class Square:
       size (int): Takes the size of the square.
     """
 
-    def __init__(self, size=0):
+
+   def __init__(self, size=0):
         """
         Constructs all the necessary attributes for the Square object.
 
@@ -21,20 +22,25 @@ class Square:
         Raises:
             TypeError: if size is not integer
             ValueError: if size is less than zero.
+            """
+        
 
-        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
 
+        
         self.__size = size
 
+    
     def area(self):
         """
         Calculates the area of the square.
 
         Returns: The square of the value of size.
         """
+        
+
         return self.__size ** 2
 
