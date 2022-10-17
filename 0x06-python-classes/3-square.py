3b7f13d72315de5453eea2a4407408f35ee8cecc
Bennet-Ukoh <bennetukoh@gmail.com>
2022-10-17 00:46:53 -0700

    3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,46 +1,30 @@
 #!/usr/bin/python3
-# 3-square.py by Bennet Ukoh
-"""Defines a square"""
-
+"""A module that defines a square """
 
 
 class Square:
-    """
-    The class defines a square.
+    """A class that represents a square"""
 
-    Attribute:
-      size (int): Takes the size of the square.
-    """
-
-
-   def __init__(self, size=0):
-        """
-        Constructs all the necessary attributes for the Square object.
-
-         Args:
+    def __init__(self, size=0):
+        """Initializing this square class
+        Args:
             size: represnets the size of the square defined
         Raises:
             TypeError: if size is not integer
-            ValueError: if size is less than zero.
-            """
-        
+            ValueError: if size is less than zero
+        """
 
         if not isinstance(size, int):
-            raise TypeError("size must be an integer")
+            raise TypeError('size must be an integer')
         if size < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError('size must be >= 0')
 
-        
         self.__size = size
 
-    
     def area(self):
         """
-        Calculates the area of the square.
-
-        Returns: The square of the value of size.
+        Calculate area of the square
+        Returns: The square of the size
         """
-        
 
-        return self.__size ** 2
-
+        return (self.__size ** 2)
