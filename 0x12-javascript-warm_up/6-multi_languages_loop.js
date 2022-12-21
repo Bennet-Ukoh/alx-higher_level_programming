5543604178c9b9182887017b59890735e0207e48
Bennet-Ukoh <bennetukoh@gmail.com>
2022-12-21 05:28:32 -0800

    code fixed

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-myVar.forEach((item) => {
-	console.log(item);
+myVar.forEach((element) => {
+  console.log(element);
 });
